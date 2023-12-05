41710b094bea27d26136feae62d3f9ff25d49a5a
Heloisa Felizardo <helocmp@yahoo.com.br>
2023-12-04 22:17:22 -0300

    add: estrutura sequencial C

---
+++ exercicios/linguagem-c/terreno/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+    double largura, comprimento, valor, area, preco;
+
+    printf("Digite a largura do terreno: ");
+    scanf("%lf", &largura);
+
+    printf("Digite o comprimento do terreno: ");
+    scanf("%lf", &comprimento);
+
+    printf("Digite o valor do metro quadrado: ");
+    scanf("%lf", &valor);
+
+    area = largura * comprimento;
+    preco = area * valor;
+
+    printf("Area do terreno = %.2lf\n", area );
+    printf("Preco do terreno = %.2lf\n", preco);
+
+    return 0;
+}
