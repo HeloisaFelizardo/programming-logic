9316ba05cd0c3d283b80e49f1e65f166bb7f9a6d
Heloisa Felizardo <helocmp@yahoo.com.br>
2023-12-07 18:03:47 -0300

    add baskara algorithm

--- exercicios/linguagem-c/baskara/main.c
+++ exercicios/linguagem-c/baskara/main.c
@@ -1,8 +1,33 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <math.h>
 
 int main()
 {
-    double a, b, c, x1, x2;
+    double a, b, c, delta, x1, x2;
+
+    printf("Coeficiente a: ");
+    scanf("%lf", &a);
+
+    printf("Coeficiente b: ");
+    scanf("%lf", &b);
+
+    printf("Coeficiente c: ");
+    scanf("%lf", &c);
+
+    delta = pow(b, 2) - 4 * a * c;
+
+    if(delta < 0 || a == 0)
+    {
+        printf("Essa equacao nao possui raizes reais.\n ");
+    }
+    else
+    {
+        x1 = (- b + sqrt(delta)) / (2 * a);
+        x2 = (- b - sqrt(delta)) / (2 * a);
+
+        printf("X1 = %.4lf\n", x1);
+        printf("X2 = %.4lf\n", x2);
+    }
+
     return 0;
 }
---
+++ exercicios/linguagem-c/menor_de_tres/main.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main()
+{
+    int n1, n2, n3, menor;
+
+    menor = 0;
+
+    printf("Primeiro valor: ");
+    scanf("%d", &n1);
+
+    printf("Segundo valor: ");
+    scanf("%d", &n2);
+
+    printf("Terceiro valor: ");
+    scanf("%d", &n3);
+
+    if(n1 < n2 && n1 < n3)
+    {
+        menor = n1;
+    }
+    else if(n2 < n1 && n2 < n3)
+    {
+        menor = n2;
+    }
+    else
+    {
+        menor = n3;
+    }
+
+    printf("MENOR = %d", menor);
+
+    return 0;
+}
